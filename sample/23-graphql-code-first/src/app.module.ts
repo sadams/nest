1f1994f2a14cf387f13f9d8ad2538d159a2a944e
Sam Adams <sam.adams@superpayments.com>
2023-03-31 09:30:33 +0100

    add throttler to gql sample to illustrate bug

--- sample/23-graphql-code-first/src/app.module.ts
+++ sample/23-graphql-code-first/src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 import { RecipesModule } from './recipes/recipes.module';
@@ -8,6 +9,10 @@ import { RecipesModule } from './recipes/recipes.module';
 @Module({
   imports: [
     RecipesModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
--- sample/23-graphql-code-first/src/recipes/recipes.module.ts
+++ sample/23-graphql-code-first/src/recipes/recipes.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DateScalar } from '../common/scalars/date.scalar';
 import { RecipesResolver } from './recipes.resolver';
 import { RecipesService } from './recipes.service';
+import {GqlThrottlerGuard} from "./throttler.guard";
 
 @Module({
-  providers: [RecipesResolver, RecipesService, DateScalar],
+  providers: [RecipesResolver, RecipesService, DateScalar, GqlThrottlerGuard],
 })
 export class RecipesModule {}
